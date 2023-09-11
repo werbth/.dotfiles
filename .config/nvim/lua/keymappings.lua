a31d2258f129a5e153ba5ca2ebe727c89c4a9dab
Werbth Rocha <werbth@gmail.com>
2023-09-11 10:23:35 -0300

    Add key mappings

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -10,6 +10,10 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { })
+vim.keymap.set('n', '<M-+>', '<C-w>+', { })
+vim.keymap.set('n', '<M-->', '<C-w>-', { })
+vim.keymap.set('n', '<M-<>', '<C-w><', { })
+vim.keymap.set('n', '<M->>', '<C-w>>', { })
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
