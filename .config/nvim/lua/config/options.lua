de8a7a28bba9ce06506741554481fb48d5975775
Werbth Rocha <werbth@gmail.com>
2024-07-29 20:37:31 -0300

    Change to pylsp

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.db_ui_save_location = "/Users/werbth/Dropbox/db_ui/"
+vim.g.lazyvim_python_lsp = "pylsp"
+
+return {}
