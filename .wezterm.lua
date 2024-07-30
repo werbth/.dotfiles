6e197e4ee86107115aa42e65e6e721d435315378
Werbth Rocha <werbth@gmail.com>
2024-07-30 10:14:55 -0300

    WezTerm config file

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 
-config.font = wezterm.font("Hack Nerd Font Mono")
-config.font_size = 15
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 16
 
 return config
