6b4708f063973b055d772118060b85265a533e16
Werbth Rocha <werbth@gmail.com>
2024-08-02 21:02:25 -0300

    Add tmux back

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,6 +5,9 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 16
+config.font_size = 15
+
+-- Disable the tab bar
+config.enable_tab_bar = false
 
 return config
