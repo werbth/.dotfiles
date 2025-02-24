39a2a9c5c5ebdb7063ea2037e0a1028d5370a30f
Werbth Rocha <werbth@gmail.com>
2025-02-24 15:27:25 -0300

    IdeaVim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.db_ui_save_location = "/Users/werbth/Dropbox/db_ui/"
-vim.g.lazyvim_python_lsp = "pylsp"
 
 return {}
